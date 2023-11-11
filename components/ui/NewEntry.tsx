c4d2f70d3d24b0589c4bc38a3f2c5f0fb4ce22bf
Martin Williner <williner.dev@gmail.com>
2023-11-11 13:42:36 -0300

    manejo de estados del formulario pt1

--- components/ui/NewEntry.tsx
+++ components/ui/NewEntry.tsx
@@ -1,3 +1,6 @@
+
+import React, { ChangeEvent, useState } from 'react'
+
 import { Button, Box,TextField } from '@mui/material'
 import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
 import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
@@ -5,44 +8,77 @@ import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOu
 
 
 export const NewEntry = () => {
+
+    const [isAdding, setIsAdding] = useState(false)
+    const [inputValue, setInputValue] = useState('')
+    const [touch, setTouch] = useState(false)
+
+    const onTextFieldChanged = (event: ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value)
+    }
+
+    const onSave = () => {
+        if(
+            inputValue.trim().length === 0
+        ) {
+            return
+        } 
+
+        console.log({ inputValue: inputValue})
+        
+    }
+
   return (
     <Box sx={{ marginBottom: 2, paddingX: 1 }}>  
 
-        <Button
-            startIcon={<AddCircleOutlineOutlinedIcon />}
-            fullWidth
-            variant='outlined'
-            color='warning'
-        >
-            Agregar nueva entrada
-        </Button>
+        {
+            isAdding ? (
+                <>
+                    <TextField
+                        fullWidth
+                        sx={{ marginBottom: 1, marginTop: 2 }}
+                        placeholder='Nueva entrada'
+                        autoFocus
+                        multiline
+                        label='Nueva entrada'
+                        helperText={ inputValue.trim().length === 0 && touch ? 'La entrada no puede estar vacia' : null}
+                        error={inputValue.trim().length === 0 && touch}
+                        value={inputValue}
+                        onChange={onTextFieldChanged}
+                        onBlur={() => setTouch(true)}
+                    />
 
-        <TextField
-            fullWidth
-            sx={{ marginBottom: 1, marginTop: 2 }}
-            placeholder='Nueva entrada'
-            autoFocus
-            multiline
-            label='Nueva entrada'
-            helperText='Escribe una tarea y presiona guardar'
-        />
+                    <Box display="flex" justifyContent="space-between">
+                        <Button
+                            variant='text'
+                            color='warning'
+                            onClick={() => setIsAdding(false)}
+                        >
+                            Cancelar
+                        </Button>
 
-        <Box display="flex" justifyContent="space-between">
-            <Button
-                variant='text'
-                color='warning'
-            >
-                Cancelar
-            </Button>
+                        <Button
+                            variant='outlined'
+                            color='secondary'
+                            endIcon={<SaveOutlinedIcon />}
+                            onClick={onSave}
+                        >
+                            Guardar
+                        </Button>
+                    </Box>
+                </>
+            ) : (<Button
+                    startIcon={<AddCircleOutlineOutlinedIcon />}
+                    fullWidth
+                    variant='outlined'
+                    color='warning'
+                    onClick={() => setIsAdding(true)}
+                >
+                    Agregar nueva entrada
+                </Button>)
+        }
 
-            <Button
-                variant='outlined'
-                color='secondary'
-                endIcon={<SaveOutlinedIcon />}
-            >
-                Guardar
-            </Button>
-        </Box>
+    
 
     </Box>
   )
